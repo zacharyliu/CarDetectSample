a3a194737e2c7cd40927752c7ada79c852671673
Zachary Liu <liu.zachary@gmail.com>
2013-07-19 14:47:58 -0400

    Realtime plotting of feature vector components.

--- src/com/zacharyliu/cardetectsample/MainActivity.java
+++ src/com/zacharyliu/cardetectsample/MainActivity.java
@@ -1,5 +1,7 @@
 package com.zacharyliu.cardetectsample;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import android.app.Activity;
@@ -7,6 +9,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
@@ -14,16 +17,23 @@ import android.view.Menu;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.androidplot.xy.BoundaryMode;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
 import com.zacharyliu.carsounddetectionlibrary.CarSoundDetectionReceiver;
 import com.zacharyliu.carsounddetectionlibrary.CarSoundDetectionService;
 import com.zacharyliu.carsounddetectionlibrary.CarSoundDetectionService.CarSoundDetectionBinder;
-import com.zacharyliu.carsounddetectionlibrary.analyzer.Result;
+import com.zacharyliu.carsounddetectionlibrary.analyzer.FeatureVector;
 
 public class MainActivity extends Activity {
 	boolean mBound = false;
 	protected CarSoundDetectionBinder mBinder;
 	private TextView resultText;
 	private ProgressBar resultBar;
+	private XYPlot graph;
+	private final int NUM_SERIES = 14;
+	private List<SimpleXYSeries> graphSeries = new ArrayList<SimpleXYSeries>(NUM_SERIES);
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +41,19 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		resultText = (TextView) findViewById(R.id.resultText);
 		resultBar = (ProgressBar) findViewById(R.id.resultBar);
+		graph = (XYPlot) findViewById(R.id.graph);
+		graph.setDomainBoundaries(0, 30, BoundaryMode.FIXED);
+//		graph.setRangeBoundaries(0, 1, BoundaryMode.FIXED);
+		
+		@SuppressWarnings("deprecation")
+		LineAndPointFormatter f1 = new LineAndPointFormatter(Color.rgb(0, 0, 200), null, Color.rgb(0, 0, 80));
+        f1.getFillPaint().setAlpha(20);
+		
+		for (int i=0; i<NUM_SERIES; i++) {
+			graphSeries.add(new SimpleXYSeries(Integer.toString(i)));
+			graphSeries.get(i).useImplicitXVals();
+			graph.addSeries(graphSeries.get(i), f1);
+		}
 		
 		// Bind to service in CarSoundDetectionLibrary
 		Intent intent = new Intent(this, CarSoundDetectionService.class);
@@ -94,12 +117,19 @@ public class MainActivity extends Activity {
 	private CarSoundDetectionReceiver callback = new CarSoundDetectionReceiver() {
 
 		@Override
-		public void onResult(Result result) {
+		public void onResult(FeatureVector vector) {
 			log("Got result");
-			Double percent = result.getResult();
+			Double percent = vector.getResult();
 			resultBar.setProgress((int) (percent * resultBar.getMax()));
-			String text = String.format(Locale.getDefault(), "%1$,.2f", percent);
+			String text = String.format(Locale.getDefault(), "[%1$,.5f, %2$,.5f]", vector.result[0], vector.result[1]);
 			resultText.setText(text);
+			for (int i=0; i<NUM_SERIES; i++) {
+				if (graphSeries.get(i).size() > 30) {
+					graphSeries.get(i).removeFirst();
+				}
+				graphSeries.get(i).addLast(null, vector.get(i));
+			}
+			graph.redraw();
 		}
 		
 	};
