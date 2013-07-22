fd04932c2dd082cedb00681afed6b3dc5fc35ddb
Zachary Liu <liu.zachary@gmail.com>
2013-07-22 10:16:30 -0400

    Add spinner to select displayed feature vector item.

--- src/com/zacharyliu/cardetectsample/MainActivity.java
+++ src/com/zacharyliu/cardetectsample/MainActivity.java
@@ -14,7 +14,12 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.androidplot.xy.BoundaryMode;
@@ -27,14 +32,16 @@ import com.zacharyliu.carsounddetectionlibrary.CarSoundDetectionService.CarSound
 import com.zacharyliu.carsounddetectionlibrary.Constants;
 import com.zacharyliu.carsounddetectionlibrary.analyzer.FeatureVector;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnItemSelectedListener {
 	boolean mBound = false;
 	protected CarSoundDetectionBinder mBinder;
 	private TextView resultText;
 	private ProgressBar resultBar;
 	private XYPlot graph;
 	private final int NUM_SERIES = Constants.NEURON_COUNTS[0];
-	private List<SimpleXYSeries> graphSeries = new ArrayList<SimpleXYSeries>(NUM_SERIES);
+	private SimpleXYSeries graphSeries;
+	private Spinner spinner;
+	private int currentDisplayPos = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,21 +53,31 @@ public class MainActivity extends Activity {
 		graph.setDomainBoundaries(0, 30, BoundaryMode.FIXED);
 //		graph.setRangeBoundaries(0, 1, BoundaryMode.FIXED);
 		
-		@SuppressWarnings("deprecation")
-		LineAndPointFormatter f1 = new LineAndPointFormatter(Color.rgb(0, 0, 200), null, Color.rgb(0, 0, 80));
-        f1.getFillPaint().setAlpha(20);
-		
-		for (int i=0; i<NUM_SERIES; i++) {
-			graphSeries.add(new SimpleXYSeries(Integer.toString(i)));
-			graphSeries.get(i).useImplicitXVals();
-			graph.addSeries(graphSeries.get(i), f1);
-		}
+		newSeries();
 		
 		// Bind to service in CarSoundDetectionLibrary
 		Intent intent = new Intent(this, CarSoundDetectionService.class);
 		bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
 		
+		// Initialize feature selector spinner
+		spinner = (Spinner) findViewById(R.id.spinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.features, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+		spinner.setOnItemSelectedListener(this);
+		
 		log("Activity created");
+	}
+	
+	private void newSeries() {
+		@SuppressWarnings("deprecation")
+		LineAndPointFormatter f1 = new LineAndPointFormatter(Color.rgb(0, 0, 200), null, Color.rgb(0, 0, 80));
+        f1.getFillPaint().setAlpha(20);
+		
+        graph.clear();
+		graphSeries = new SimpleXYSeries("Feature");
+		graphSeries.useImplicitXVals();
+		graph.addSeries(graphSeries, f1);
 	}
 
 	@Override
@@ -124,12 +141,10 @@ public class MainActivity extends Activity {
 			resultBar.setProgress((int) (percent * resultBar.getMax()));
 			String text = String.format(Locale.getDefault(), "[%1$,.5f, %2$,.5f]", vector.result[0], vector.result[1]);
 			resultText.setText(text);
-			for (int i=0; i<NUM_SERIES; i++) {
-				if (graphSeries.get(i).size() > 30) {
-					graphSeries.get(i).removeFirst();
-				}
-				graphSeries.get(i).addLast(null, vector.get(i));
+			if (graphSeries.size() > 30) {
+				graphSeries.removeFirst();
 			}
+			graphSeries.addLast(null, vector.get(currentDisplayPos));
 			graph.redraw();
 		}
 		
@@ -137,6 +152,20 @@ public class MainActivity extends Activity {
 	
 	public static void log(String message) {
 		Log.d("Message!", message);
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int pos,
+			long id) {
+		// Set the graph to display the specified position in the feature vector
+		currentDisplayPos = pos;
+		newSeries();
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
